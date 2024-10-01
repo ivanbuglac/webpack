0437a5fd4af7f065c6f3344a2259d5083f5887d4
Ivan <ivanbuglac@gmail.com>
2024-10-01 20:32:56 +0300

    first webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.html'
+import './index.scss'
+
+import code from './img/dog.jpg'
+
+const image = new Image()
+image.src = code
+const mult = (a, b) => a + b
+console.log(mult(4, 5))
